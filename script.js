ecf4bca3609319193f7574d02cb678b25ae521f1
reachmohdaltaf <reachmohdaltaf@gmail.com>
2024-10-19 12:06:58 +0530

    newwwww

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-document.querySelector('.searchbar .fa').addEventListener('click', function() {
-    const searchInput = document.querySelector('.searchbar input');
-    searchInput.classList.toggle('active'); 
-    searchInput.focus(); 
+document.querySelector(".searchbar .fa").addEventListener("click", function () {
+  const searchInput = document.querySelector(".searchbar input");
+  searchInput.classList.toggle("active");
+  searchInput.focus();
 });
-
-
-
-
-
-
-
-
-
 
 const searchinput = document.getElementById("inputbox");
 const formsubmit = document.getElementById("myform");
@@ -24,47 +15,31 @@ const weatherimage = document.getElementById("image1");
 const tempreature = document.getElementById("temperature");
 
 const description = document.getElementById("description");
-const wind = document.getElementById("windspeed") 
-const humid = document.getElementById("humid") 
+const wind = document.getElementById("windspeed");
+const humid = document.getElementById("humid");
 
+async function checkWeather(city) {
+  const api_key = "e5708a84eab866cd2eb9f08daf2f4be3";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
-async function checkWeather(city){
-        const api_key = "e5708a84eab866cd2eb9f08daf2f4be3"
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
+  try {
+    const response = await fetch(url);
+    const weather = await response.json(); // Corrected here
+    console.log(weather);
 
-    try {
-        const response = await fetch(url);
-        const weather = await response.json(); // Corrected here
-        console.log(weather);
-
-        tempreature.innerText = weather.main.temp;
-        description.innerText = weather.weather[0].description;
-        wind.innerText = weather.wind.speed;
-        humid.innerText = weather.main.humidity;
-
-
-        
-
-        
-
-    } catch (error) {
-        console.error("Error fetching the weather data:", error);
-    }
+    tempreature.innerText = weather.main.temp;
+    description.innerText = weather.weather[0].description;
+    wind.innerText = weather.wind.speed;
+    humid.innerText = weather.main.humidity;
+  } catch (error) {
+    console.error("Error fetching the weather data:", error);
+  }
 }
 
+formsubmit.addEventListener("submit", function (event) {
+  event.preventDefault();
+  checkWeather(searchinput.value);
 
-
-
-
-formsubmit.addEventListener("submit", function(event){
-    event.preventDefault();
-    checkWeather(searchinput.value);
-
-    const name= document.getElementById("name")
-    name.innerText = searchinput.value
-
-
-})
-
-
-  
+  const name = document.getElementById("name");
+  name.innerText = searchinput.value;
+});
